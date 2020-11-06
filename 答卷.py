bf820de12b81036c966da859feb4dfd98e64b176
performer <15815094902@163.com>
2020-11-06 18:28:32 +0800

    存储到CSV文件中

--- 答卷.py
+++ 答卷.py
@@ -1,31 +1,68 @@
+import csv
 import json
-
+import time
+import jsonpath
 import requests
 
-url = "https://shuyang.anjuke.com/map/sale/"
+
+for i in range(0, 9, 1):
+    # time.sleep(10)
+    url = "https://shuyang.anjuke.com/v3/ajax/map/sale/708/prop_list/?room_num=%s" % i
+    print(url)
+
+    # ip = "117.94.120.41"
+    # post = 4285
+    #
+    # proxies = {
+    #   "http": "http://%s:%d" % (ip, post),
+    #   "https": "http://%s:%d" % (ip, post)
+    # }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+    }
+
+    r = requests.get(url, headers=headers)
+
+    info_list = r.json()
+
+    itme_list = jsonpath.jsonpath(info_list, "$.val.props")
+    # print(itme_list)
+    all_house = []
+
+    header = ['id', 'region_name', 'block_name', 'rhval', 'area',
+                  'long_title', 'comm_name', 'floor_tag', 'fitment_value']
+
+    with open("沭阳房产_20201106.cav", "w") as f:
+        # 创建一个csv的DictWriter对象，这样才能够将写入csv格式数据到这个文件
+        f_csv = csv.DictWriter(f, header)
+        # 写入一行（我们用第一行当做表头）
+        f_csv.writeheader()
 
 
-# ip = "60.169.134.146"
-# post = 4245
-#
-# proxies = {
-#   "http": "http://%s:%d" % (ip, post),
-#   "https": "http://%s:%d" % (ip, post)
-# }
+    for i in itme_list[0]:
+        # time.sleep(2)
+        id = i.get("id")
+        region_name = i.get("region_name")
+        block_name = i.get("block_name")
+        rhval = i.get("rhval")
+        area = i.get("area")
+        long_title = i.get("long_title")
+        comm_name = i.get("comm_name")
+        floor_tag = i.get("floor_tag")
+        fitment_value = i.get("fitment_value")
 
-headers = {
-    "user-aengt": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-}
+        dict0 = {'id': id, 'region_name': region_name, 'block_name': block_name, 'rhval': rhval, 'area': area,
+                 'long_title': long_title, 'comm_name': comm_name, 'floor_tag': floor_tag, 'fitment_value': fitment_value}
 
-r = requests.get(url=url, headers=headers)
+        all_house.append(dict0)
+        # print(all_house)
 
-with open("wangye.html", "wb") as f:
-    f.write(r.content)
+    n = r.url
+    n = n.replace("https://shuyang.anjuke.com/v3/ajax/map/sale/708/prop_list/?room_num=", "")
 
-
-json_str = r.content.decode()
-
-json_dict = json.loads(json_str)
-
-
-
+    with open("沭阳房产_20201106(%s).cav" % n, "w", encoding="utf-8") as f:
+        # 创建一个csv的DictWriter对象，这样才能够将写入csv格式数据到这个文件
+        f_csv = csv.DictWriter(f, header)
+        # 写入多行行（当做数据)
+        f_csv.writerows(all_house)
